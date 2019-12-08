2a3f9f44dbbe43dc6203f568029539adf1fa7c1b
HenintsoaRaza <henintsoa.razafindrazaka@edu.ece.fr>
2019-12-08 18:01:52 +0100

    boot marche pas

--- lab2/index.js
+++ lab2/index.js
@@ -1,10 +1,16 @@
 express = require('express')
 app = express()
 
+path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('port', 1337)
 
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs')
+
 app.get('/hello/:name', 
-(req, res) => res.send("Hello " + req.params.name)
+(req, res) => res.render('hello.ejs', {name: req.params.name})
   )
   
   app.post('/', (req, res) => {
