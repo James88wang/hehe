06607fae41ab6d6be3e83d2bba83e6f384fb7651
James <james.wang@edu.ece.fr>
2019-12-09 02:08:10 +0100

    lab3.0

---
+++ lab3.0/src/server.ts
@@ -0,0 +1,80 @@
+import express = require('express')
+import { Metric, MetricsHandler } from './metrics'
+import path = require('path')
+import bodyparser = require('body-parser')
+
+
+const app = express()
+const port: string = process.env.PORT || '8082'
+app.use(express.static(path.join(__dirname, '/../public')))
+
+app.set('views', __dirname + "/../views")
+app.set('view engine', 'ejs');
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
+
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+app.post('/metrics/:id', (req: any, res: any) => {
+  dbMet.save(req.params.id, req.body, (err: Error | null) => {
+    if (err) throw err
+    res.status(200).send('ola que tal ?')
+  })
+})
+
+app.get('/metrics/getAll', (req: any, res: any) => {
+  dbMet.getAll( 
+    (
+      err: Error | null, result?: any
+    ) => {
+      if (err) throw err
+      console.log("we try to get All metrics")
+      res.status(200).send(result)
+  })
+})
+
+app.get('/metric/getOne', (req: any, res: any) => {
+  dbMet.getOne( 
+    (
+      err: Error | null, result?: any
+      ) => {
+        console.log("we try to get one metric")
+        res.status(200).send(result)
+    })
+})
+
+/*app.get('/metric/del/:key', (req: any, res: any) => {
+  dbMet.del( 
+    (
+      key: string,
+      
+      console.log("we try to get All metrics")
+      res.status(200).send(result)
+  })
+})*/
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.get('/hello/:name', (req: any, res: any) => {
+  res.render('hello.ejs', {name: req.params.name})
+})
+
+app.get('/metrics.json', (req: any, res: any) => {
+  MetricsHandler.get((err: Error | null, result?: any) => {
+    if (err) {
+      throw err
+    }
+    res.json(result)
+  })
+})
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Server is running on http://localhost:${port}`)
+})
